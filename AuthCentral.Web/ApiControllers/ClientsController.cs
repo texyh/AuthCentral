71626d5a8a2ff4f336926748dbbb2f99fcdaf573
texyh <onwuzulikee1@gmail.com>
2018-03-28 20:53:19 +0100

    fixed isue

--- AuthCentral.Web/ApiControllers/ClientsController.cs
+++ AuthCentral.Web/ApiControllers/ClientsController.cs
@@ -15,7 +15,7 @@ namespace AuthCentral.Web.ApiControllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
