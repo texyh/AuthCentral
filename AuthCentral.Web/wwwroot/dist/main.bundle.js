c4bd015108d669abad88115d5777c5b4cf35eb86
texyh <onwuzulikee1@gmail.com>
2018-04-11 22:16:44 +0100

    made changes

--- AuthCentral.Web/wwwroot/dist/main.bundle.js
+++ AuthCentral.Web/wwwroot/dist/main.bundle.js
@@ -17,21 +17,135 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 
 /***/ }),
 
-/***/ "./src/app/app.component.css":
-/***/ (function(module, exports) {
+/***/ "./src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-module.exports = "\r\n.container-1{\r\n    background-color: #2D323F;\r\n    height:100%;\r\n    position: fixed;\r\n    bottom: 0;\r\n}\r\n\r\n.box1 ul{\r\n    display:block; \r\n    width: 100%;\r\n    padding: 10px 0px 10px 0px;\r\n    text-align: left;    \r\n}\r\n\r\n.box1 ul li{\r\n    display:block;\r\n    border: none; \r\n    height: 40px;\r\n    padding-top: 5px;\r\n    padding-bottom: 5px;\r\n     \r\n}\r\n\r\n.box1 ul li a{\r\n    text-decoration: none;\r\n    list-style: none;\r\n    color: white;\r\n    display: block;\r\n    padding-top:7px;\r\n    padding-bottom: 7px;\r\n    padding-left: 7px;\r\n        \r\n   \r\n}\r\n\r\n.box1 ul li a:hover{\r\n    display: block;\r\n    background-color: #424652;\r\n  \r\n   \r\n}\r\n\r\n.container-2{\r\n    background-color: darkgrey;\r\n}\r\n\r\n.sidebar{\r\n    width:25%;\r\n    height:100%;\r\n    background:gray;\r\n}"
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_router__ = __webpack_require__("./node_modules/@angular/router/esm5/router.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__nav_nav_component__ = __webpack_require__("./src/app/nav/nav.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__layout_app_component__ = __webpack_require__("./src/app/layout/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__clients_clients_component__ = __webpack_require__("./src/app/clients/clients.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__resources_resources_component__ = __webpack_require__("./src/app/resources/resources.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__users_users_component__ = __webpack_require__("./src/app/users/users.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__angular_common__ = __webpack_require__("./node_modules/@angular/common/esm5/common.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__layout_app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_3__nav_nav_component__["a" /* NavComponent */],
+                __WEBPACK_IMPORTED_MODULE_5__clients_clients_component__["a" /* ClientsComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__resources_resources_component__["a" /* ResourcesComponent */],
+                __WEBPACK_IMPORTED_MODULE_7__users_users_component__["a" /* UsersComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_router__["a" /* RouterModule */].forRoot([
+                    { path: '', redirectTo: 'clients', pathMatch: 'full' },
+                    { path: 'clients', component: __WEBPACK_IMPORTED_MODULE_5__clients_clients_component__["a" /* ClientsComponent */] },
+                    { path: 'resources', component: __WEBPACK_IMPORTED_MODULE_6__resources_resources_component__["a" /* ResourcesComponent */] },
+                    { path: 'users', component: __WEBPACK_IMPORTED_MODULE_7__users_users_component__["a" /* UsersComponent */] },
+                    { path: '**', redirectTo: 'clients' }
+                ])
+            ],
+            providers: [
+                { provide: __WEBPACK_IMPORTED_MODULE_8__angular_common__["g" /* LocationStrategy */], useClass: __WEBPACK_IMPORTED_MODULE_8__angular_common__["d" /* HashLocationStrategy */] }
+            ],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__layout_app_component__["a" /* AppComponent */]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
 
 /***/ }),
 
-/***/ "./src/app/app.component.html":
+/***/ "./src/app/clients/clients.component.css":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=row>\r\n    <div class=\"box col-sm-12\" >\r\n        <div class=\"container-1 col-sm-3 \">\r\n          <div class=\"box1\">\r\n              <ul>\r\n                  <li><a href=\"#\"> Dashboard </a></li>\r\n                  <li><a href=\"#\"> Dashboard</a> </li>\r\n                  <li><a href=\"#\"> Dashboard </a></li>\r\n      \r\n              </ul>\r\n          </div>\r\n        </div>\r\n          <div class=\"container-2 col-sm-9\">\r\n      \r\n          </div>\r\n      </div>\r\n      \r\n</div>\r\n\r\n\r\n"
+module.exports = ""
 
 /***/ }),
 
-/***/ "./src/app/app.component.ts":
+/***/ "./src/app/clients/clients.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  clients works!\n</p>\n"
+
+/***/ }),
+
+/***/ "./src/app/clients/clients.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ClientsComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var ClientsComponent = /** @class */ (function () {
+    function ClientsComponent() {
+    }
+    ClientsComponent.prototype.ngOnInit = function () {
+    };
+    ClientsComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-clients',
+            template: __webpack_require__("./src/app/clients/clients.component.html"),
+            styles: [__webpack_require__("./src/app/clients/clients.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], ClientsComponent);
+    return ClientsComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/layout/app.component.css":
+/***/ (function(module, exports) {
+
+module.exports = "\r\ndiv.spa {\r\n    bottom: 0;\r\n    margin-top: 50px;\r\n}\r\n\r\n\r\n"
+
+/***/ }),
+
+/***/ "./src/app/layout/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "\r\n<div class=\"row\">\r\n<div class=\"spa\">\r\n<div class=\"col-md-3\">\r\n    <app-nav></app-nav>\r\n</div>\r\n<div class=\"col-md-9\">\r\n    <router-outlet></router-outlet>\r\n</div>\r\n</div>\r\n</div>"
+
+/***/ }),
+
+/***/ "./src/app/layout/app.component.ts":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -49,10 +163,10 @@ var AppComponent = /** @class */ (function () {
         this.title = 'app';
     }
     AppComponent = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
             selector: 'app-root',
-            template: __webpack_require__("./src/app/app.component.html"),
-            styles: [__webpack_require__("./src/app/app.component.css")]
+            template: __webpack_require__("./src/app/layout/app.component.html"),
+            styles: [__webpack_require__("./src/app/layout/app.component.css")]
         })
     ], AppComponent);
     return AppComponent;
@@ -62,39 +176,150 @@ var AppComponent = /** @class */ (function () {
 
 /***/ }),
 
-/***/ "./src/app/app.module.ts":
+/***/ "./src/app/nav/nav.component.css":
+/***/ (function(module, exports) {
+
+module.exports = "ul.side-nav {\r\n    background-color: #2D323F;\r\n    height:100%;\r\n    position: fixed;\r\n}\r\n\r\n\r\n.side-nav {\r\n    display:block; \r\n    width: 25%;\r\n    padding: 10px 0px 10px 0px;\r\n    text-align: left;    \r\n}\r\n\r\n\r\n.side-nav  li{\r\n    display:block;\r\n    \r\n    height: 40px;\r\n    padding-top: 5px;\r\n    padding-bottom: 5px;\r\n     \r\n}\r\n\r\n\r\n.side-nav  li a{\r\n    text-decoration: none;\r\n    list-style: none;\r\n    color: white;\r\n    display: block;\r\n    padding-top:7px;\r\n    padding-bottom: 7px;\r\n    padding-left: 7px;\r\n        \r\n   \r\n}\r\n\r\n\r\n.side-nav  li a:hover{\r\n    display: block;\r\n    background-color: #424652;\r\n  \r\n}"
+
+/***/ }),
+
+/***/ "./src/app/nav/nav.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<ul class=\"side-nav\">\n    <li>\n        <a [routerLinkActive]=\"['active']\" [routerLink]=\"['clients']\"> Clients </a>\n    </li>\n    <li>\n        <a [routerLinkActive]=\"['active']\" [routerLink]=\"['resources']\"> Resources</a>\n    </li>\n    <li>\n        <a [routerLinkActive]=\"['active']\" [routerLink]=\"['users']\"> Users </a>\n    </li>\n</ul>"
+
+/***/ }),
+
+/***/ "./src/app/nav/nav.component.ts":
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/platform-browser.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("./src/app/app.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return NavComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 
-
-
-var AppModule = /** @class */ (function () {
-    function AppModule() {
+var NavComponent = /** @class */ (function () {
+    function NavComponent() {
     }
-    AppModule = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
-            declarations: [
-                __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
-            ],
-            imports: [
-                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
-            ],
-            providers: [],
-            bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
-        })
-    ], AppModule);
-    return AppModule;
+    NavComponent.prototype.ngOnInit = function () {
+    };
+    NavComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-nav',
+            template: __webpack_require__("./src/app/nav/nav.component.html"),
+            styles: [__webpack_require__("./src/app/nav/nav.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], NavComponent);
+    return NavComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/resources/resources.component.css":
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/resources/resources.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  resources works!\n</p>\n"
+
+/***/ }),
+
+/***/ "./src/app/resources/resources.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ResourcesComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var ResourcesComponent = /** @class */ (function () {
+    function ResourcesComponent() {
+    }
+    ResourcesComponent.prototype.ngOnInit = function () {
+    };
+    ResourcesComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-resources',
+            template: __webpack_require__("./src/app/resources/resources.component.html"),
+            styles: [__webpack_require__("./src/app/resources/resources.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], ResourcesComponent);
+    return ResourcesComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/users/users.component.css":
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/users/users.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  users works!\n</p>\n"
+
+/***/ }),
+
+/***/ "./src/app/users/users.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return UsersComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var UsersComponent = /** @class */ (function () {
+    function UsersComponent() {
+    }
+    UsersComponent.prototype.ngOnInit = function () {
+    };
+    UsersComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-users',
+            template: __webpack_require__("./src/app/users/users.component.html"),
+            styles: [__webpack_require__("./src/app/users/users.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], UsersComponent);
+    return UsersComponent;
 }());
 
 
@@ -131,7 +356,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* enableProdMode */])();
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_12" /* enableProdMode */])();
 }
 Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
     .catch(function (err) { return console.log(err); });
